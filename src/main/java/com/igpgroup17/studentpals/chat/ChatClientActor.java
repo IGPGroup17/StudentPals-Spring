5ed80cb28100803ac8e6e480e430818e34c187fa
OllieJonas <olliejonas@gmail.com>
2021-03-10 18:21:23 +0000

    WELL APPARENTLY IT DOESNT GUESS IM DOING AKKA WITH JAVA THEN

---
+++ src/main/java/com/igpgroup17/studentpals/chat/ChatClientActor.java
@@ -0,0 +1,4 @@
+package com.igpgroup17.studentpals.chat;
+
+public class ChatClientActor {
+}
--- src/main/java/com/igpgroup17/studentpals/chat/ChatRoomActor.scala
+++
@@ -1,7 +0,0 @@
-package com.igpgroup17.studentpals.chat
-
-class ChatRoomActor {
-  def helloWorld(): String = {
-    "Hello from Scala!"
-  }
-}
